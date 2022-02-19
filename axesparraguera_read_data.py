c5414cff0196902e9a54e7d33039b75dd7dd1a23
arturxe2 <arturxe@gmail.com>
2022-02-19 11:36:21 +0100

    Update axesparraguera_read_data.py

--- axesparraguera_read_data.py
+++ axesparraguera_read_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import json
 
 path = '/data-net/datasets/SoccerNetv2/data_split/'
 init_path = '/data-net/datasets/SoccerNetv2/ResNET_TF2/'
@@ -12,10 +13,21 @@ print(init_path + 'england_epl/2014-2015/2015-04-11 - 19-30 Burnley 0 - 1 Arsena
 print(os.listdir(init_path + 'england_epl/2014-2015/2015-04-11 - 19-30 Burnley 0 - 1 Arsenal/'))
 
 i = 0
+chunks = 60
+n_total = 0
 for line in lines:
     i += 1
     print(line)
-    features = np.load(init_path + line.rstrip('\n') + '/1_ResNET_TF2.npy')
-    print(features.shape)
+    features1 = np.load(init_path + line.rstrip('\n') + '/1_ResNET_TF2.npy')
+    features2 = np.load(init_path + line.rstrip('\n') + '/2_ResNET_TF2.npy')
+    n_chunks1 = int(features1.shape[0] // chunks)
+    n_chunks2 = int(features2.shape[0] // chunks)
+    n_total = n_total + n_chunks1 + n_chunks2
+    actions = pd.DataFrame(json.load(open(init_path + line.rstrip('\n') + '/Labels-v2.json'))['annotations'])
+    actions['half'] = actions['gameTime'].apply(lambda x: int(x[0]))
+    actions['minute'] = actions['gameTime'].apply(lambda x: x[4:])
+    actions['frame'] = actions['minute'].apply(lambda x: int(x[0:2]) * 60 * 2 + int(x[3:5]) * 2)
+    print(actions.head())
+    print(features1.shape)
     if i == 2:
         break
