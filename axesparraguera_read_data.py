c4be3d40f851a02fb9e6a020c31f857877f06e49
arturxe2 <arturxe@gmail.com>
2022-02-19 11:28:06 +0100

    Update axesparraguera_read_data.py

--- axesparraguera_read_data.py
+++ axesparraguera_read_data.py
@@ -8,6 +8,7 @@ init_path = '/data-net/datasets/SoccerNetv2/ResNET_TF2/'
 with open(path + 'train.txt') as f:
     lines = f.readlines()
     
+print(init_path + 'england_epl/2014-2015/2015-04-11 - 19-30 Burnley 0 - 1 Arsenal/')
 print(os.listdir(init_path + 'england_epl/2014-2015/2015-04-11 - 19-30 Burnley 0 - 1 Arsenal/'))
 
 i = 0
