fa8af9e4a1e56398ef1738b6f5af27170bd46ab5
arturxe2 <arturxe@gmail.com>
2022-02-19 10:12:53 +0100

    Update axesparraguera_read_data.py

--- axesparraguera_read_data.py
+++ axesparraguera_read_data.py
@@ -1,6 +1,17 @@
+import pandas as pd
+import numpy as np
+
 path = '/data-net/datasets/SoccerNetv2/data_split/'
+init_path = '/data-net/datasets/SoccerNetv2/ResNET_TF2'
+
 with open(path + 'train.txt') as f:
     lines = f.readlines()
 
+i = 0
 for line in lines:
+    i += 1
     print(line)
+    features = np.load(init_path + line + '1_ResNet_TF2.npy')
+    print(features.shape)
+    if i == 2:
+        break
