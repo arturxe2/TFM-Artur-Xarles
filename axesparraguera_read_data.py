37edb90b1e7111ba34d9448a08f4f5b33396ae50
arturxe2 <arturxe@gmail.com>
2022-02-19 12:05:48 +0100

    Update axesparraguera_read_data.py

--- axesparraguera_read_data.py
+++ axesparraguera_read_data.py
@@ -57,7 +57,7 @@ def read_data_train(chunks = 60):
     
         #Print the number of the match we are
         print('Data collected for ' + str(i) + ' matches.')
-        if i == 2:
+        if i == 100:
             break
     
     #Resize data, and put output in one-hot-encoding
