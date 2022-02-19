3f4175c7b01ffdae6f30fb0654d6eec2c3e53b93
arturxe2 <arturxe@gmail.com>
2022-02-19 11:00:50 +0100

    Update axesparraguera_read_data.py

--- axesparraguera_read_data.py
+++ axesparraguera_read_data.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+import os
 
 path = '/data-net/datasets/SoccerNetv2/data_split/'
 init_path = '/data-net/datasets/SoccerNetv2/ResNET_TF2/'
 
 with open(path + 'train.txt') as f:
     lines = f.readlines()
+    
+print(os.listdir(init_path + 'england_epl/2014_2015'))
 
 i = 0
 for line in lines:
