a5ef22ff912613cf8a69fb4021be14492a31461d
arturxe2 <arturxe@gmail.com>
2022-02-19 18:12:38 +0100

    Update axesparraguera_read_data.py

--- axesparraguera_read_data.py
+++ axesparraguera_read_data.py
@@ -8,8 +8,7 @@ import tensorflow as tf
 
 def my_loss(y_true, y_pred):
     y_true = tf.cast(y_true, tf.float32)
-    #loss = tf.reduce_mean(y_true * y_pred + (1 - y_true) * y_pred)
-    loss = tf.reduce_mean(tf.reduce_mean(tf.math.maximum(y_true, tf.keras.backend.epsilon()) * -tf.math.log(tf.math.maximum(y_pred, tf.keras.backend.epsilon())) + (1-y_true) * (-tf.math.log(tf.math.maximum(1 - y_pred, tf.keras.backend.epsilon()))), axis = 1))
+    loss = tf.reduce_mean(tf.reduce_mean(tf.math.maximum(y_true, tf.keras.backend.epsilon()) * -tf.math.log(tf.math.maximum(y_pred, tf.keras.backend.epsilon())) + (1-y_true) * (-tf.math.log(tf.math.maximum(1 - y_pred, tf.keras.backend.epsilon()))), axis = 0))
     return loss
 
 def read_data(chunks = 60, data_split = "train"):
