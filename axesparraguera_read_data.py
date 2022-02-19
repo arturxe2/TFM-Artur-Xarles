29873723e1728ba6a5e066d4caf41d4a6be22271
arturxe2 <arturxe@gmail.com>
2022-02-19 12:03:42 +0100

    Update axesparraguera_read_data.py

--- axesparraguera_read_data.py
+++ axesparraguera_read_data.py
@@ -4,66 +4,74 @@ import os
 import json
 from sklearn.preprocessing import MultiLabelBinarizer
 
-path = '/data-net/datasets/SoccerNetv2/data_split/'
-init_path = '/data-net/datasets/SoccerNetv2/ResNET_TF2/'
-
-with open(path + 'train.txt') as f:
-    lines = f.readlines()
+def read_data_train(chunks = 60):
+    #Initialize values
+    i = 0
+    n_total = 0
+    y_train = ['Elements']
+    X = []
+    #Define train matches directories
+    path = '/data-net/datasets/SoccerNetv2/data_split/'
+    init_path = '/data-net/datasets/SoccerNetv2/ResNET_TF2/'
+    with open(path + 'train.txt') as f:
+        lines = f.readlines()
+    #Read data for each match
+    for line in lines:
+        i += 1
+        #1 -> 1st half, 2 -> 2nd half
+        #Load .npy files
+        features1 = np.load(init_path + line.rstrip('\n') + '/1_ResNET_TF2.npy')
+        features2 = np.load(init_path + line.rstrip('\n') + '/2_ResNET_TF2.npy')
+        #Define number of chunks second splits
+        n_chunks1 = int(features1.shape[0] // chunks)
+        n_chunks2 = int(features2.shape[0] // chunks)
+        n_total = n_total + n_chunks1 + n_chunks2
+        #Read actions for match and determine the nº of frame it corresponds
+        actions = pd.DataFrame(json.load(open(init_path + line.rstrip('\n') + '/Labels-v2.json'))['annotations'])
+        actions['half'] = actions['gameTime'].apply(lambda x: int(x[0]))
+        actions['minute'] = actions['gameTime'].apply(lambda x: x[4:])
+        actions['frame'] = actions['minute'].apply(lambda x: int(x[0:2]) * 60 * 2 + int(x[3:5]) * 2)
+        #Split data in 60-frames chunks (for 1st half)
+        for n in range(n_chunks1):
+            #Collect features
+            x = features1[n * chunks : (n + 1) * chunks, :]
+            X.append(x.tolist())
+            #Collect outputs
+            y = actions['label'][(actions['frame'] >= n * chunks) & (actions['frame'] < (n + 1) * chunks) & (actions['half'] == 1)].values
+            if len(y) == 0:
+                y = ['Background']
+            else:
+                y = y.tolist()
+            y_train.append(y)
+            
+        #Split data in 60-frame chunks (for 2nd half)
+        for n in range(n_chunks2):
+            x = features2[n * chunks : (n + 1) * chunks, :]
+            X.append(x.tolist())
+            y = actions['label'][(actions['frame'] >= n * chunks) & (actions['frame'] < (n + 1) * chunks) & (actions['half'] == 2)].values
+            if len(y) == 0:
+                y = ['Background']
+            else:
+                y = y.tolist()
+            y_train.append(y)
     
-print(init_path + 'england_epl/2014-2015/2015-04-11 - 19-30 Burnley 0 - 1 Arsenal/')
-print(os.listdir(init_path + 'england_epl/2014-2015/2015-04-11 - 19-30 Burnley 0 - 1 Arsenal/'))
+        #Print the number of the match we are
+        print('Data collected for ' + str(i) + ' matches.')
+        if i == 2:
+            break
+    
+    #Resize data, and put output in one-hot-encoding
+    X = np.array(X).reshape(n_total, chunks, features1.shape[1])
+    y_train = y_train[1:]
+    aux = pd.Series(y_train)
+    mlb = MultiLabelBinarizer()
+    res = pd.DataFrame(mlb.fit_transform(aux), columns = mlb.classes_, index = aux.index)
+    classes = res.columns
+    res = np.array(res)
 
-i = 0
-chunks = 60
-n_total = 0
-y_train = ['Elements']
-X = []
-for line in lines:
-    i += 1
-    print(line)
-    features1 = np.load(init_path + line.rstrip('\n') + '/1_ResNET_TF2.npy')
-    features2 = np.load(init_path + line.rstrip('\n') + '/2_ResNET_TF2.npy')
-    n_chunks1 = int(features1.shape[0] // chunks)
-    print(features1.shape)
-    n_chunks2 = int(features2.shape[0] // chunks)
-    n_total = n_total + n_chunks1 + n_chunks2
-    actions = pd.DataFrame(json.load(open(init_path + line.rstrip('\n') + '/Labels-v2.json'))['annotations'])
-    actions['half'] = actions['gameTime'].apply(lambda x: int(x[0]))
-    actions['minute'] = actions['gameTime'].apply(lambda x: x[4:])
-    actions['frame'] = actions['minute'].apply(lambda x: int(x[0:2]) * 60 * 2 + int(x[3:5]) * 2)
-    for n in range(n_chunks1):
-        x = features1[n * chunks : (n + 1) * chunks, :]
-        X.append(x.tolist())
-        y = actions['label'][(actions['frame'] >= n * chunks) & (actions['frame'] < (n + 1) * chunks) & (actions['half'] == 1)].values
-        if len(y) == 0:
-            y = ['']
-        else:
-            y = y.tolist()
-        y_train.append(y)
-        
+    return X, res, classes
 
-    for n in range(n_chunks2):
-        x = features2[n * chunks : (n + 1) * chunks, :]
-        X.append(x.tolist())
-        y = actions['label'][(actions['frame'] >= n * chunks) & (actions['frame'] < (n + 1) * chunks) & (actions['half'] == 2)].values
-        if len(y) == 0:
-            y = ['']
-        else:
-            y = y.tolist()
-        y_train.append(y)
-
-
-    print(i)
-    if i == 2:
-        break
-
-X = np.array(X).reshape(n_total, chunks, features1.shape[1])
-y_train = y_train[1:]
-aux = pd.Series(y_train)
-mlb = MultiLabelBinarizer()
-res = pd.DataFrame(mlb.fit_transform(aux), columns = mlb.classes_, index = aux.index)
-classes = res.columns
-res = np.array(res)
-print(X.shape)
-print(res.shape)
+x_train, y_train, classes = read_data_train(chunks = 120)
+print(x_train.shape)
+print(y_train.shape)
 print(classes)
