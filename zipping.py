a96718cd39ac11f03665eb1f8d9eab606cdad854
arturxe2 <arturxe@gmail.com>
2022-02-23 22:04:44 +0100

    Create zipping.py

---
+++ zipping.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 22:03:45 2022
+
+@author: artur
+"""
+
+import shutil
+
+shutil.make_archive('Results.zip', 'zip', '/home-net/axesparraguera/data/test_predictions')
+
