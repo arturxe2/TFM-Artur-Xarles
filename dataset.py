66ed58d959e5464130eddf03ac47cbb68121abef
arturxe2 <arturxe@gmail.com>
2022-03-01 21:53:36 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -148,6 +148,7 @@ class SoccerNetClips(Dataset):
                 print('asdfkjlñasdfkjasñldf')
                 if half == 1:
                     print('asdf')
+                    print(a- self.size//stride +1)
                     label_half1[(a - self.size//stride + 1) : (a + 1)][0] = 0 # not BG anymore
                     print('asdfkñljasdf')
                     label_half1[(a - self.size//stride + 1) : (a + 1)][label+1] = 1 # that's my class
