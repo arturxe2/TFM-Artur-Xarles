102d44dd03e2c7eef382cae94f5ae73a7635f670
arturxe2 <arturxe@gmail.com>
2022-03-12 21:46:47 +0100

    Update axesparraguera_main.py

--- axesparraguera_main.py
+++ axesparraguera_main.py
@@ -65,7 +65,7 @@ def main(args):
     if not args.test_only:
         criterion = NLLLoss()
         optimizer = torch.optim.Adam(model.parameters(), lr=args.LR, 
-                                    betas=(0.7, 0.999), eps=1e-08, 
+                                    betas=(0.9, 0.999), eps=1e-08, 
                                     weight_decay=0, amsgrad=True)
         #optimizer = torch.optim.SGD(model.parameters(), lr=args.LR,
         #                            momentum=0.9)
