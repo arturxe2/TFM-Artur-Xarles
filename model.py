bb8190bceed02cb52629a16d850633902ebcbbc6
arturxe2 <arturxe@gmail.com>
2022-03-16 18:54:48 +0100

    Update model.py

--- model.py
+++ model.py
@@ -83,11 +83,11 @@ class Model(nn.Module):
             self.relu = nn.ReLU()
             #Add segment embedding
             self.pos_encoder = PositionalEncoding(512, )
-            encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
+            encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=4, dim_feedforward=64)
             self.encoder = nn.TransformerEncoder(encoder_layer, 1)
-            encoder_layer2 = nn.TransformerEncoderLayer(d_model=512, nhead=8)
+            encoder_layer2 = nn.TransformerEncoderLayer(d_model=512, nhead=4, dim_feedforward=64)
             self.encoder2 = nn.TransformerEncoder(encoder_layer2, 1)
-            encoder_layer3 = nn.TransformerEncoderLayer(d_model=512, nhead=8)
+            encoder_layer3 = nn.TransformerEncoderLayer(d_model=512, nhead=4, dim_feedforward=64)
             self.encoder3 = nn.TransformerEncoder(encoder_layer3, 1)
             #Pool layer
             self.pool_layer = nn.MaxPool1d(chunk_size * (2 + 1), stride=1)
