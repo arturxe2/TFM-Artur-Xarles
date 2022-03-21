504d307ba88dbb2ab24e8d7e1dbb7bbee11cc161
arturxe2 <arturxe@gmail.com>
2022-03-21 16:20:33 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -32,11 +32,13 @@ def mix_up(feat1, feat2, y1, y2):
 def augmentation(features, y, n_aug = 1):
     features = np.array(features)
     y = np.array(y)
+    print(y.shape)
     weights = np.array([0, 1/0.88, 1/0.76, 1/0.79, 1/0.70, 1/0.56, 1/0.58, 
                         1/0.58, 1/0.71, 1/0.87, 1/0.85, 1/0.77, 1/0.62, 
                         1/0.69, 1/0.89, 1/0.69, 1/0.08, 1/0.19])
+    print(weights.shape)
     prob_ind = y.dot(weights)
-    print(prob_ind)
+    breakpoint()
     i=0
     feat_aug_list = []
     y_aug_list = []
