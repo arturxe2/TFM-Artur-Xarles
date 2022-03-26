3b3b3645dfe9f0fa72ad2622d4c453694a4e8c54
arturxe2 <arturxe@gmail.com>
2022-03-26 18:45:21 +0100

    Update model.py

--- model.py
+++ model.py
@@ -131,8 +131,35 @@ class Model(nn.Module):
             self.pool_layerB = nn.MaxPool1d(chunk_size * (1), stride=1)
             self.pool_layerR = nn.MaxPool1d(chunk_size * 2, stride = 1)
             self.fc = nn.Linear(1024, self.num_classes+1)
+        
+        elif self.pool == "transformer_2features_3":
+            self.conv1R = nn.Conv1d(512, 512, 1, stride=1, bias=False)
+            self.normR = nn.BatchNorm1d(512)
+            self.conv1B1 = nn.Conv1d(2048, 512, 1, stride=1, bias=False)
+            self.normB1 = nn.BatchNorm1d(512)
+            self.conv1B2 = nn.Conv1d(2048, 512, 1, stride=1, bias=False)
+            self.normB2 = nn.BatchNorm1d(512)
+            self.conv1B3 = nn.Conv1d(384, 512, 1, stride=1, bias=False)
+            self.normB3 = nn.BatchNorm1d(512)
+            self.conv1B4 = nn.Conv1d(2048, 512, 1, stride=1, bias=False)
+            self.normB4 = nn.BatchNorm1d(512)
+            self.conv1B5 = nn.Conv1d(2048, 512, 1, stride=1, bias=False)
+            self.normB5 = nn.BatchNorm1d(512)
+            self.relu = nn.ReLU()
+            #Add segment embedding
+            self.pos_encoder = PositionalEncoding(512, )
             
+            encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=4, dim_feedforward=64)
+            self.encoder = nn.TransformerEncoder(encoder_layer, 1)
             
+            encoder_layer2 = nn.TransformerEncoderLayer(d_model=512, nhead=4, dim_feedforward=64)
+            self.encoder2 = nn.TransformerEncoder(encoder_layer2, 1)
+            
+            encoder_layer3 = nn.TransformerEncoderLayer(d_model=512, nhead=4, dim_feedforward=64)
+            self.encoder3 = nn.TransformerEncoder(encoder_layer3, 1)
+            #Pool layer
+            self.pool_layer = nn.MaxPool1d(chunk_size * (2 + 5), stride=1)
+            self.fc2 = nn.Linear(512, self.num_classes+1)
 
         elif self.pool == "NetVLAD":
             self.pool_layer = NetVLAD(num_clusters=64, dim=512,
@@ -261,6 +288,48 @@ class Model(nn.Module):
             
             outputs = self.sigm(self.fc(self.drop(inputs)))
             
+        elif self.pool == "transformer_2features_3":
+            inputs1 = inputs1.float()
+            inputs2 = inputs2.float()
+            inputsB = inputs1.permute((0, 2, 1))
+            inputsR = inputs2.permute((0, 2, 1))
+            inputsR = self.relu(self.normR(self.conv1R(inputsR)))#(B x 512 x (chunk_size * 2))
+            inputsB1 = inputsB[:, :2048, :]
+            inputsB2 = inputsB[:, 2048:4096, :]
+            inputsB3 = inputsB[:, 4096:4480, :]
+            inputsB4 = inputsB[:, 4480:6528, :]
+            inputsB5 = inputsB[:, 6528:8576, :]
+            inputsB1 = self.relu(self.normB1(self.conv1B1(inputsB1))) #(B x 512 x (chunk_size))
+            inputsB2 = self.relu(self.normB2(self.conv1B2(inputsB2))) #(B x 512 x (chunk_size))
+            inputsB3 = self.relu(self.normB3(self.conv1B3(inputsB3))) #(B x 512 x (chunk_size))
+            inputsB4 = self.relu(self.normB4(self.conv1B4(inputsB4))) #(B x 512 x (chunk_size))
+            inputsB5 = self.relu(self.normB5(self.conv1B5(inputsB5))) #(B x 512 x (chunk_size))
+            inputsR = inputsR.permute((0, 2, 1))#(B x (chunk_size * 2) x 512)
+            inputsB1 = inputsB1.permute((0, 2, 1))#(B x (chunk_size) x 512)
+            inputsB2 = inputsB1.permute((0, 2, 1))#(B x (chunk_size) x 512)
+            inputsB3 = inputsB1.permute((0, 2, 1))#(B x (chunk_size) x 512)
+            inputsB4 = inputsB1.permute((0, 2, 1))#(B x (chunk_size) x 512)
+            inputsB5 = inputsB1.permute((0, 2, 1))#(B x (chunk_size) x 512)
+            
+            #Positional encodding + feature encoding (1 for R, 0 for B)
+            inputsR = self.pos_encoder(inputsR, add=0.)#(B x (chunk_size * 2) x 512)
+            inputsB1 = self.pos_encoder(inputsB1, add=1.)#(B x (chunk_size) x 512)
+            inputsB2 = self.pos_encoder(inputsB2, add=2.)#(B x (chunk_size) x 512)
+            inputsB3 = self.pos_encoder(inputsB3, add=3.)#(B x (chunk_size) x 512)
+            inputsB4 = self.pos_encoder(inputsB4, add=4.)#(B x (chunk_size) x 512)
+            inputsB5 = self.pos_encoder(inputsB5, add=5.)#(B x (chunk_size) x 512)
+
+            
+            inputs = torch.cat((inputsR, inputsB1, inputsB2, inputsB3, inputsB4, inputsB5), dim=1) #(B x (chunk_size * (1 + 2)) x 512)
+            #Encoders
+            inputs = self.encoder(inputs) #(B x (chunk_size * (1 + 2)) x 512)
+            inputs = self.encoder2(inputs)
+            inputs = self.encoder3(inputs)
+            
+            inputs = inputs.permute((0, 2, 1))
+            inputs_pooled = self.pool_layer(inputs)
+            inputs_pooled = inputs_pooled.squeeze(-1)
+            outputs = self.sigm(self.fc2(self.drop(inputs_pooled)))
             
             
             
