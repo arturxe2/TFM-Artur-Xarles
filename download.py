9b8554c4ebf2c7764d7fc77593d9d160410339e8
arturxe2 <arturxe@gmail.com>
2022-04-04 15:52:10 +0200

    Update download.py

--- download.py
+++ download.py
@@ -5,7 +5,7 @@ from SoccerNet.Downloader import SoccerNetDownloader
 mySoccerNetDownloader = SoccerNetDownloader(
     LocalDirectory="/data-net/datasets/SoccerNetv2/videos_lowres")
 
-mySoccerNetDownloader.password = 's0cc3rn3t'
+mySoccerNetDownloader.password = input("Password for videos?:\n")
 
 # download LQ (224p) Videos
 mySoccerNetDownloader.downloadGames(files=["1_224p.mkv", "2_224p.mkv"], split=["train","valid","test","challenge"])
