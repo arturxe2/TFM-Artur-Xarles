de66c0ab66f3cf7a85699f38f943e87258bd1831
arturxe2 <arturxe@gmail.com>
2022-04-05 21:58:56 +0200

    Update model.py

--- model.py
+++ model.py
@@ -200,7 +200,7 @@ class Model(nn.Module):
             print("=> loaded checkpoint '{}' (epoch {})"
                   .format(weights, checkpoint['epoch']))
     #def forward(self, inputs):
-    def forward(self, inputs):
+    def forward(self, inputs1, inputs2):
         # input_shape: (batch,frames,dim_features)
 
         # Temporal pooling operation
