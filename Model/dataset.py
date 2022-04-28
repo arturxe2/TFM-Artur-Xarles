3b396d002109f678888d530f9fdf3f5c4ed7205d
arturxe2 <arturxe@gmail.com>
2022-04-28 19:07:22 +0200

    Update dataset.py

--- Model/dataset.py
+++ Model/dataset.py
@@ -316,7 +316,7 @@ class SoccerNetClipsTrain(Dataset):
                  path_audio = '/home-net/axesparraguera/data/VGGFeatures', 
                  path_labels = "/data-net/datasets/SoccerNetv2/ResNET_TF2", 
                  features_baidu = 'baidu_soccer_embeddings.npy', 
-                 features_audio = 'VGGish.npy', stride = 1, split=["train"], version=1, 
+                 features_audio = 'VGGish.npy', stride = 1, split=["train"], version=2, 
                 framerate=1, chunk_size=7, augment = False):
         
         self.path_baidu = path_baidu
