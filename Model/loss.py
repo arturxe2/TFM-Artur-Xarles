e39b4713765495e7c3006217c9a0e9b6d126751e
arturxe2 <arturxe@gmail.com>
2022-04-29 20:19:15 +0200

    a

--- Model/loss.py
+++ Model/loss.py
@@ -9,10 +9,10 @@ class NLLLoss(torch.nn.Module):
         return torch.mean(torch.mean(labels * -torch.log(output) + (1 - labels) * -torch.log(1 - output)))
     
 class NLLLoss_weights(torch.nn.Module):
-    def __init__(self, weights1):
+    def __init__(self):
         super(NLLLoss_weights, self).__init__()
-        self.weights1 = weights1
+        #self.weights1 = weights1
         
     def forward(self, labels, output):
-        #weights = torch.tensor([1, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]).cuda()
+        weights = torch.tensor([1, 20, 10, 15, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 15, 20, 20]).cuda()
         return torch.mean(torch.mean(self.weights1 * labels * -torch.log(output) + (1 - labels) * -torch.log(1 - output), dim=0))
