757b6ec428ee7a6f0367c067bd759288dbfdffe3
arturxe2 <arturxe@gmail.com>
2022-05-07 13:08:07 +0200

    Update vggish_train.py

--- VGGish_features/vggish_train.py
+++ VGGish_features/vggish_train.py
@@ -133,3 +133,27 @@ if __name__ == '__main__':
         batch_size=128, pin_memory=True)
     print(model)
     
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+    
+            # zero the parameter gradients
+            optimizer.zero_grad()
+    
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+    
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+print('Finished Training')
+    
