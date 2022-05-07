89c90f296fa499d3957407d28d3e40ada11c30e1
arturxe2 <arturxe@gmail.com>
2022-05-07 20:10:37 +0200

    Update vggish_train.py

--- VGGish_features/vggish_train.py
+++ VGGish_features/vggish_train.py
@@ -89,7 +89,7 @@ class TrainVGGish(Dataset):
         i = 0
         for game in tqdm(self.listGames):
             i += 1
-            if i < 10:
+            if i < 1000:
 
                 # Load features
                 feat_half1 = np.load(os.path.join(self.path, game, "1_" + self.features))
