d7992bf56cafd90b2dd21ad64e95f66dc80f75a2
arturxe2 <arturxe@gmail.com>
2022-05-09 18:45:40 +0200

    Update vggish_post.py

--- VGGish_features/vggish_post.py
+++ VGGish_features/vggish_post.py
@@ -40,5 +40,15 @@ model_name = 'model'
 model = VGGish(urls = model_urls, pretrained = True, preprocess = False, postprocess=False).cuda()
 checkpoint = torch.load(os.path.join("models", model_name, "model.pth.tar"))
 model.load_state_dict(checkpoint['state_dict'])
+model.classifier = Postprocessor()
+state_dict = hub.load_state_dict_from_url(urls['pca'], progress=progress)
+    # TODO: Convert the state_dict to torch
+state_dict[vggish_params.PCA_EIGEN_VECTORS_NAME] = torch.as_tensor(
+    state_dict[vggish_params.PCA_EIGEN_VECTORS_NAME], dtype=torch.float
+)
+state_dict[vggish_params.PCA_MEANS_NAME] = torch.as_tensor(
+    state_dict[vggish_params.PCA_MEANS_NAME].reshape(-1, 1), dtype=torch.float
+)
+model.classifier.load_state_dict(state_dict)
 
 print(model)
