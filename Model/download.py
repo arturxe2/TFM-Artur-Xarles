b9d017642a6b48720b2f1664d7e054aef2ef8767
arturxe2 <arturxe@gmail.com>
2022-05-10 16:33:43 +0200

    Update download.py

--- Model/download.py
+++ Model/download.py
@@ -3,6 +3,7 @@ import SoccerNet
 from SoccerNet.Downloader import SoccerNetDownloader, getListGames
 import os
 import numpy as np
+from moviepy.editor import VideoFileClip
 
 path = "/data-net/datasets/SoccerNetv2/videos_lowres"
 mySoccerNetDownloader = SoccerNetDownloader(
@@ -22,5 +23,24 @@ for split in splits:
         if (audio1.shape[0] == 0) | (audio2.shape[0] == 0):
             print('Downloading game: ' + game)
             mySoccerNetDownloader.downloadGame(game, files = ["1_224p.mkv", "2_224p.mkv"], spl = split)
+            
+            print('Extracting 1st half audio...')
+            clip = VideoFileClip(os.path.join(path, game, '1_224p.mkv'))
+            clip.audio.write_audiofile(os.path.join(path, game, '1_224.wav'))
+            os.remove(os.path.join(path, game, '1_224.mkv'))
+            
+            
+            print('Extracting 2nd half audio...')
+            clip = VideoFileClip(os.path.join(path, game, '2_224p.mkv'))
+            clip.audio.write_audiofile(os.path.join(path, game, '2_224.wav'))
+            os.remove(os.path.join(path, game, '2_224.mkv'))
+            
+        else:
+            if os.path.exists(os.path.join(path, game, '1_224.mp3')):
+                print('Removing mp3 audio 1st half')
+                os.remove(os.path.join(path, game, '1_224.mp3'))
+            if os.path.exists(os.path.join(path, game, '2_224.mp3')):
+                print('Removing mp3 audio 2nd half')
+                os.remove(os.path.join(path, game, '2_224.mp3'))
 #mySoccerNetDownloader.downloadGame(files=["1_224p.mkv", "2_224p.mkv"], split=["train","valid","test","challenge"])
 
