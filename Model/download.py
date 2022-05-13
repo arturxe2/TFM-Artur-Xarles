0006385bc4612e7b416a84f5dbc22587b2107b12
arturxe2 <arturxe@gmail.com>
2022-05-13 14:59:01 +0200

    Update download.py

--- Model/download.py
+++ Model/download.py
@@ -4,7 +4,6 @@ from SoccerNet.Downloader import SoccerNetDownloader, getListGames
 import os
 import numpy as np
 from moviepy.editor import VideoFileClip
-from pydub import AudioSegment
 
 path = "/data-local/data1-hdd/axesparraguera/vggish"
 mySoccerNetDownloader = SoccerNetDownloader(
@@ -31,24 +30,23 @@ for split in splits:
             audio2 = np.load(os.path.join(path, game, '2_audio.npy'))
             print(audio2.shape)
             if (audio1.shape[0] == 0) | (audio2.shape[0] == 0):
-                print('Incorrect .npy shape')
-                print('Downloading game: ' + game)
-                mySoccerNetDownloader.downloadGame(game, files = ["1_224p.mkv", "2_224p.mkv"], spl = split)
-                
-                print('Extracting 1st half audio...')
-                clip = VideoFileClip(os.path.join(path, game, '1_224p.mkv'))
-                clip.audio.write_audiofile(os.path.join(path, game, '1_224.mp3'))
-                sound = AudioSegment.from_mp3(os.path.join(path, game, '1_224.mp3'))
-                sound.export(os.path.join(path, game, "1_audio.wav"), format="wav")
-                os.remove(os.path.join(path, game, '1_224.mkv'))
-                
-                
-                print('Extracting 2nd half audio...')
-                clip = VideoFileClip(os.path.join(path, game, '2_224p.mkv'))
-                clip.audio.write_audiofile(os.path.join(path, game, '2_224.mp3'))
-                sound = AudioSegment.from_mp3(os.path.join(path, game, '2_224.mp3'))
-                sound.export(os.path.join(path, game, "2_audio.wav"), format="wav")
-                os.remove(os.path.join(path, game, '2_224.mkv'))
+                try:
+                    print('Incorrect .npy shape')
+                    print('Downloading game: ' + game)
+                    mySoccerNetDownloader.downloadGame(game, files = ["1_224p.mkv", "2_224p.mkv"], spl = split)
+                    
+                    print('Extracting 1st half audio...')
+                    clip = VideoFileClip(os.path.join(path, game, '1_224p.mkv'))
+                    clip.audio.write_audiofile(os.path.join(path, game, '1_224.wav'))
+                    os.remove(os.path.join(path, game, '1_224.mkv'))
+                    
+                    
+                    print('Extracting 2nd half audio...')
+                    clip = VideoFileClip(os.path.join(path, game, '2_224p.mkv'))
+                    clip.audio.write_audiofile(os.path.join(path, game, '2_224.wav'))
+                    os.remove(os.path.join(path, game, '2_224.mkv'))
+                except:
+                    print('Error extracting audio from mkv file\n\n añlkjsdfajsdflkasdfj')
                 
             else:
                 if os.path.exists(os.path.join(path, game, '1_224p.mp3')):
@@ -59,23 +57,23 @@ for split in splits:
                     os.remove(os.path.join(path, game, '2_224p.mp3'))
             
         else:
-            print('There is not audio.npy file for this match')
-            print('Downloading game: ' + game)
-            mySoccerNetDownloader.downloadGame(game, files = ["1_224p.mkv", "2_224p.mkv"], spl = split)
-            
-            print('Extracting 1st half audio...')
-            clip = VideoFileClip(os.path.join(path, game, '1_224p.mkv'))
-            clip.audio.write_audiofile(os.path.join(path, game, '1_224.mp3'))
-            sound = AudioSegment.from_mp3(os.path.join(path, game, '1_224.mp3'))
-            sound.export(os.path.join(path, game, "1_audio.wav"), format="wav")
-            os.remove(os.path.join(path, game, '1_224.mkv'))
-            
-            
-            print('Extracting 2nd half audio...')
-            clip = VideoFileClip(os.path.join(path, game, '2_224p.mkv'))
-            clip.audio.write_audiofile(os.path.join(path, game, '2_224.mp3'))
-            sound = AudioSegment.from_mp3(os.path.join(path, game, '2_224.mp3'))
-            sound.export(os.path.join(path, game, "2_audio.wav"), format="wav")
-            os.remove(os.path.join(path, game, '2_224.mkv'))
+            try:
+                print('There is not audio.npy file for this match')
+                print('Downloading game: ' + game)
+                mySoccerNetDownloader.downloadGame(game, files = ["1_224p.mkv", "2_224p.mkv"], spl = split)
+                
+                print('Extracting 1st half audio...')
+                clip = VideoFileClip(os.path.join(path, game, '1_224p.mkv'))
+                clip.audio.write_audiofile(os.path.join(path, game, '1_224.wav'))
+                os.remove(os.path.join(path, game, '1_224.mkv'))
+                
+                
+                print('Extracting 2nd half audio...')
+                clip = VideoFileClip(os.path.join(path, game, '2_224p.mkv'))
+                clip.audio.write_audiofile(os.path.join(path, game, '2_224.wav'))
+                os.remove(os.path.join(path, game, '2_224.mkv'))
+                
+            except:
+                print('Error extracting audio from mkv file\n\n añlkjsdfajsdflkasdfj')
 #mySoccerNetDownloader.downloadGame(files=["1_224p.mkv", "2_224p.mkv"], split=["train","valid","test","challenge"])
 
