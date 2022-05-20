7acda61e9370de0322d3890e91e93bcf55019f80
arturxe2 <arturxe@gmail.com>
2022-05-20 17:20:49 +0200

    a

--- Model/axesparraguera_main.py
+++ Model/axesparraguera_main.py
@@ -129,7 +129,7 @@ def main(args):
         ensemble = True
         
         if ensemble:
-            results = testSpottingEnsemble(args.SoccerNet_path, args.model_name, split, ensemble_method='mean')
+            results = testSpottingEnsemble(args.SoccerNet_path, args.model_name, split, ensemble_method='weighted_mean')
         
         else:
             dataset_Test  = SoccerNetClipsTesting(path=args.SoccerNet_path, features=args.features, split=[split], version=args.version, framerate=args.framerate, chunk_size=args.chunk_size*args.framerate)
