4d6585635c0f1b2c64055ffd1dbfd7cba0adfe58
arturxe2 <arturxe@gmail.com>
2022-05-20 16:42:38 +0200

    Update train.py

--- Model/train.py
+++ Model/train.py
@@ -670,7 +670,7 @@ def testSpotting(path, dataloader, model, model_name, overwrite=True, NMS_window
   
 def testSpottingEnsemble(path, model_name, split, overwrite=True, NMS_window=30, NMS_threshold=0.5, ensemble_method = 'best4class'):
 
-    split2 = '_'.join(split)
+    split2 = '_'.join([split])
     chunk_sizes = [2, 3]#, 4, 5, 7]
     
     # print(split)
