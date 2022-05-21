8d3a48fc935fb9fc50e07db13e53b863ffd9eccf
arturxe2 <arturxe@gmail.com>
2022-05-21 12:24:49 +0200

    Update loss.py

--- VGGish_features/loss.py
+++ VGGish_features/loss.py
@@ -17,3 +17,13 @@ class NLLLoss_weights(torch.nn.Module):
         weights = torch.tensor([1, 20, 10, 15, 10, 20, 20, 20, 10, 10, 10, 10, 10, 10, 10, 15, 30, 50]).cuda()
         #weights = torch.tensor([1, 40, 20, 30, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 30, 80, 80]).cuda()
         return torch.mean(torch.mean(weights * labels * -torch.log(output) + (1 - labels) * -torch.log(1 - output), dim=0))
+
+class NLLLoss_weights2(torch.nn.Module):
+    def __init__(self):
+        super(NLLLoss_weights, self).__init__()
+        #self.weights1 = weights1
+        
+    def forward(self, labels, output):
+        weights = torch.tensor([1, 20, 10, 15, 10, 20, 20, 20, 10, 10, 10, 10, 10, 10, 10, 30, 30, 50]).cuda()
+        #weights = torch.tensor([1, 40, 20, 30, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 30, 80, 80]).cuda()
+        return torch.mean(torch.mean(weights * labels * -torch.log(output) + (1 - labels) * -torch.log(1 - output), dim=0))
