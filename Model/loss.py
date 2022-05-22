3e57d2703b1f36f344db0c86689560b158966aae
arturxe2 <arturxe@gmail.com>
2022-05-22 10:58:01 +0200

    Update loss.py

--- Model/loss.py
+++ Model/loss.py
@@ -36,7 +36,7 @@ class NLLLoss_weights_ensemble(torch.nn.Module):
         #self.weights1 = weights1
         
     def forward(self, labels, output):
-        weights = torch.tensor([80, 40, 60, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 60, 150, 150]).cuda() #loss online
+        weights = torch.tensor([160, 80, 120, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 120, 300, 300]).cuda() #loss online
         #weights = torch.tensor([1, 20, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 20, 20]).cuda() #loss ram
         #weights = torch.tensor([1, 40, 20, 30, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 30, 80, 80]).cuda()
         return torch.mean(torch.mean(weights * labels * -torch.log(output) + (1 - labels) * -torch.log(1 - output), dim=0))
