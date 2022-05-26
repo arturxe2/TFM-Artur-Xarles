e33c78ced2d6963a41350122d289bb3b41110c26
arturxe2 <arturxe@gmail.com>
2022-05-26 08:31:25 +0200

    Update store_data.py

--- Model/store_data.py
+++ Model/store_data.py
@@ -1,6 +1,6 @@
 from dataset import SoccerNetClips, SoccerNetClipsTesting, SoccerNetClipsTrain
 
-a = SoccerNetClipsTrain(store = True, chunk_size = 4, stride = 2, split = ['train', 'valid', 'test'])
+a = SoccerNetClipsTrain(store = True, chunk_size = 5, stride = 2, split = ['train', 'valid', 'test'])
 print(a.__len__())
 print(a.__getitem__(0))
 
